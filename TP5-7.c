332c3f8718c460f89e3986ec02499a4b84ced1fe
avrusf <136859745+avrusf@users.noreply.github.com>
2023-06-23 20:26:22 -0300

    Add files via upload

---
+++ TP5-7.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+int factor(int num){
+	int factorial=1;
+	
+	for (int i=1; i<=num; i++){
+		factorial = factorial * i;
+	}
+	
+	printf ("\n El factorial es: %d", factorial);
+	
+	return 0;
+}
+
+int main() {
+	int num=0;
+	
+	printf ("Ingrese un numero:\n");
+	scanf ("%d", &num);
+	
+	factor(num);
+	
+	return 0;
+}
+
